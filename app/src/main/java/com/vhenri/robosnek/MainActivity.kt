0581d326ab34fd9358d478fd5c6e331b89da0b6e
vhenri <victoriabhenri@gmail.com>
2022-11-01 12:17:42 -0400

    - init android project w/ Android Studio compose template

---
+++ app/src/main/java/com/vhenri/robosnek/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.vhenri.robosnek
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vhenri.robosnek.ui.theme.RobosnekTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RobosnekTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RobosnekTheme {
+        Greeting("Android")
+    }
+}
